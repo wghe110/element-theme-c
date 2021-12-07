89bc7894a3ecc4971ed94d191f00c4cf7ca91153
wanggonghe <wanggonghe@cestc.cn>
2021-12-07 16:04:24 +0800

    fix(package):修改依赖包

--- src/views/button.vue
+++ src/views/button.vue
@@ -5,7 +5,6 @@
       <li>
         <el-button>Default</el-button>
         <el-button type="primary">Primary</el-button>
-        <el-button type="primary-gradient">Primary-gradient</el-button>
         <el-button type="success">Success</el-button>
         <el-button type="info">Info</el-button>
         <el-button type="warning">Warning</el-button>

8ea89c7c32cbcf5d99340238e82f5b4fed51c122
wanggonghe <wanggonghe@cestc.cn>
2021-12-07 10:29:31 +0800

    fix(scss):更新scss文件成最新的

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,20 @@
 'use strict';
 
-const { series, src, dest, watch } = require('gulp');
+const { series, src, dest } = require('gulp');
 const sass = require('gulp-dart-sass');
 const autoprefixer = require('gulp-autoprefixer');
 const cssmin = require('gulp-cssmin');
 
 function compile() {
-  return src('./src/element-theme/*.scss')
+  return src('./src/theme-chalk/*.scss')
     .pipe(sass.sync().on('error', sass.logError))
     .pipe(autoprefixer({
       overrideBrowserslist: ['ie > 9', 'last 2 versions'],
       cascade: false
     }))
     .pipe(cssmin())
-    .pipe(dest('./public/element-theme'));
+    .pipe(dest('./dist'));
 }
 
-function copyfont() {
-  return src('./src/element-theme/fonts/**')
-    .pipe(cssmin())
-    .pipe(dest('./public/element-theme/fonts'));
-}
 
-function watchScss() {
-  watch(['/src/element-theme/*.scss'], function (cb) {
-    series(compile, copyfont);
-    cb();
-  })
-}
-
-exports.build = series(compile, copyfont);
-exports.watch = series(watchScss);
+exports.build = series(compile);

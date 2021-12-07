488b75a854b2f6a7d5f3593d3e894d3d1178bf83
wanggonghe <wanggonghe@cestc.cn>
2021-12-07 14:23:16 +0800

    fix(init):修改基础颜色

--- src/main.ts
+++ src/main.ts
@@ -4,17 +4,22 @@ import App from './App.vue';
 // normalize.css
 import 'normalize.css'
 
+//公共样式
+import './style/reset.scss'
+import './style/layout.scss'
+
 // elementUI plus
 import ElementPlus from 'element-plus'
 // 引入基础的样式
-import './element-theme/menu.scss'
-import './element-theme/base.scss'
-import './element-theme/col.scss'
-import './element-theme/header.scss'
-import './element-theme/container.scss'
-import './element-theme/aside.scss'
-import './element-theme/footer.scss'
-import './element-theme/main.scss'
+import './theme-chalk/menu.scss'
+import './theme-chalk/base.scss'
+import './theme-chalk/col.scss'
+import './theme-chalk/header.scss'
+import './theme-chalk/container.scss'
+import './theme-chalk/aside.scss'
+import './theme-chalk/footer.scss'
+import './theme-chalk/main.scss'
+
 
 // router
 import router from './router/index'
--- src/views/button.vue
+++ src/views/button.vue
@@ -1,55 +1,231 @@
 <template>
-  <div>
-    <el-row style="margin-bottom: 16px">
-      <el-button>Default</el-button>
-      <el-button type="primary">Primary</el-button>
-      <el-button type="success">Success</el-button>
-      <el-button type="info">Info</el-button>
-      <el-button type="warning">Warning</el-button>
-      <el-button type="danger">Danger</el-button>
-      <el-button>中文</el-button>
-    </el-row>
+  <div class="item">
+    <h2>基本用法</h2>
+    <ul class="list">
+      <li>
+        <el-button>Default</el-button>
+        <el-button type="primary">Primary</el-button>
+        <el-button type="primary-gradient">Primary-gradient</el-button>
+        <el-button type="success">Success</el-button>
+        <el-button type="info">Info</el-button>
+        <el-button type="warning">Warning</el-button>
+        <el-button type="danger">Danger</el-button>
+        <el-button>中文</el-button>
+      </li>
+      <li>
+        <el-button plain>Plain</el-button>
+        <el-button type="primary" plain>Primary</el-button>
+        <el-button type="success" plain>Success</el-button>
+        <el-button type="info" plain>Info</el-button>
+        <el-button type="warning" plain>Warning</el-button>
+        <el-button type="danger" plain>Danger</el-button>
+      </li>
+      <li>
+        <el-button round>Round</el-button>
+        <el-button type="primary" round>Primary</el-button>
+        <el-button type="success" round>Success</el-button>
+        <el-button type="info" round>Info</el-button>
+        <el-button type="warning" round>Warning</el-button>
+        <el-button type="danger" round>Danger</el-button>
+      </li>
+      <li>
+        <el-button  circle>
+          <el-icon>
+            <Search />
+          </el-icon>
+        </el-button>
+        <el-button type="primary"  circle>
+          <el-icon>
+            <Edit />
+          </el-icon>
+        </el-button>
+        <el-button type="success" circle>
+          <el-icon>
+            <Check />
+          </el-icon>
+        </el-button>
+        <el-button type="info" circle>
+          <el-icon>
+            <Message />
+          </el-icon>
+        </el-button>
+        <el-button type="warning" circle>
+          <el-icon>
+            <Star />
+          </el-icon>
+        </el-button>
+        <el-button type="danger" circle>
+          <el-icon>
+            <Delete />
+          </el-icon>
+        </el-button>
+      </li>
+    </ul>
+  </div>
 
-    <el-row style="margin-bottom: 16px">
-      <el-button plain>Plain</el-button>
-      <el-button type="primary" plain>Primary</el-button>
-      <el-button type="success" plain>Success</el-button>
-      <el-button type="info" plain>Info</el-button>
-      <el-button type="warning" plain>Warning</el-button>
-      <el-button type="danger" plain>Danger</el-button>
-    </el-row>
+  <div class="item">
+    <h2>禁用状态</h2>
+    <ul class="list">
+      <li>
+        <el-button disabled>Default</el-button>
+        <el-button type="primary" disabled>Primary</el-button>
+        <el-button type="success" disabled>Success</el-button>
+        <el-button type="info" disabled>Info</el-button>
+        <el-button type="warning" disabled>Warning</el-button>
+        <el-button type="danger" disabled>Danger</el-button>
+      </li>
+      <li>
+        <el-button plain disabled>Plain</el-button>
+        <el-button type="primary" plain disabled>Primary</el-button>
+        <el-button type="success" plain disabled>Success</el-button>
+        <el-button type="info" plain disabled>Info</el-button>
+        <el-button type="warning" plain disabled>Warning</el-button>
+        <el-button type="danger" plain disabled>Danger</el-button>
+      </li>
+    </ul>
+  </div>
 
-    <el-row style="margin-bottom: 16px">
-      <el-button round>Round</el-button>
-      <el-button type="primary" round>Primary</el-button>
-      <el-button type="success" round>Success</el-button>
-      <el-button type="info" round>Info</el-button>
-      <el-button type="warning" round>Warning</el-button>
-      <el-button type="danger" round>Danger</el-button>
-    </el-row>
+  <div class="item">
+    <h2>文字按钮</h2>
+    <ul class="list">
+      <li>
+        <el-button type="text">Text Button</el-button>
+        <el-button type="text" disabled>Text Button</el-button>
+      </li>
+    </ul>
+  </div>
 
-    <el-row style="margin-bottom: 16px">
-      <el-button  circle>
-        <el-icon>
-          <search />
-        </el-icon>
-      </el-button>
-      <el-button type="primary" :icon="Edit" circle></el-button>
-      <el-button type="success" :icon="Check" circle></el-button>
-      <el-button type="info" :icon="Message" circle></el-button>
-      <el-button type="warning" :icon="Star" circle></el-button>
-      <el-button type="danger" :icon="Delete" circle></el-button>
-    </el-row>
+  <div class="item">
+    <h2>图标按钮</h2>
+    <ul class="list">
+      <li>
+        <el-button type="primary">
+          <el-icon>
+            <Edit/>
+          </el-icon>
+        </el-button>
+        <el-button type="primary">
+          <el-icon>
+            <Share />
+          </el-icon>
+        </el-button>
+        <el-button type="primary">
+          <el-icon>
+            <Delete />
+          </el-icon>
+        </el-button>
+        <el-button type="primary">
+          <el-icon>
+            <Search />
+          </el-icon>
+          <span>Search</span>
+        </el-button>
+        <el-button type="primary">
+          <span>Upload</span>
+          <el-icon>
+            <Upload />
+          </el-icon>
+        </el-button>
+      </li>
+    </ul>
+  </div>
+
+  <div class="item">
+    <h2>按钮组</h2>
+    <ul class="list">
+      <li>
+        <el-button-group>
+          <el-button type="primary">
+            <el-icon>
+              <ArrowLeft />
+            </el-icon>
+            Previous Page
+          </el-button>
+          <el-button type="primary">
+            Next Page
+            <el-icon><ArrowRight /></el-icon>
+          </el-button>
+        </el-button-group>
+
+        <el-button-group style="margin-left: 14px;">
+          <el-button type="primary" >
+            <el-icon>
+              <Edit/>
+            </el-icon>
+          </el-button>
+          <el-button type="primary">
+            <el-icon>
+              <Share/>
+            </el-icon>
+          </el-button>
+          <el-button type="primary">
+            <el-icon>
+              <Delete/>
+            </el-icon>
+          </el-button>
+        </el-button-group>
+      </li>
+    </ul>
+  </div>
+
+  <div class="item">
+    <h2>加载中</h2>
+    <ul class="list">
+      <li>
+        <el-button type="primary" :loading="true">Loading</el-button>
+      </li>
+    </ul>
+  </div>
+
+  <div class="item">
+    <h2>各种尺寸的尺寸按钮</h2>
+    <ul class="list">
+      <li>
+        <el-button>Default</el-button>
+        <el-button size="medium">Medium</el-button>
+        <el-button size="small">Small</el-button>
+        <el-button size="mini">Mini</el-button>
+      </li>
+      <li>
+        <el-button round>Default</el-button>
+        <el-button size="medium" round>Medium</el-button>
+        <el-button size="small" round>Small</el-button>
+        <el-button size="mini" round>Mini</el-button>
+      </li>
+      <li>
+        <el-button circle>
+          <el-icon>
+            <Search />
+          </el-icon>
+        </el-button>
+        <el-button :icon="Search" size="medium" circle>
+          <el-icon>
+            <Search />
+          </el-icon>
+        </el-button>
+        <el-button size="small" circle>
+          <el-icon>
+            <Search />
+          </el-icon>
+        </el-button>
+        <el-button size="mini" circle>
+          <el-icon>
+            <Search />
+          </el-icon>
+        </el-button>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-  import { Search, Edit, Check, Message, Star, Delete } from '@element-plus/icons'
-  import '../element-theme/button.scss'
+  import { Search, Edit, Check, Message, Star, Delete, Share, Upload, ArrowLeft, ArrowRight } from '@element-plus/icons'
+  import '../theme-chalk/button.scss'
+  import '../theme-chalk/button-group.scss'
 
   export default {
     components: {
-      Search, Edit, Check, Message, Star, Delete
+      Search, Edit, Check, Message, Star, Delete, Share, Upload, ArrowLeft, ArrowRight
     }
   }
 </script>
